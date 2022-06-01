e2c4981b8f30761f43aa6e1c739c01ea0f6aa746
Hanri <henry018@laposte.net>
2022-06-01 10:59:30 +0200

    small fixes

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:pixel_war_app/pixel_grid_widget.dart';
+import 'package:pixel_war_app/widgets/pixel_grid_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: PixelGridWidget(),
+    return Scaffold(
+      body: PixelGridWidget(parentContext: context),
     );
   }
 }
--- lib/widgets/pixel_grid_widget.dart
+++ lib/widgets/pixel_grid_widget.dart
@@ -3,10 +3,11 @@ import 'package:pixel_war_app/models/pixel_model.dart';
 import 'package:pixel_war_app/widgets/pixel_widget.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
-import 'helpers/helpers.dart';
+import '../helpers/helpers.dart';
 
 class PixelGridWidget extends StatefulWidget {
-  const PixelGridWidget({Key? key}) : super(key: key);
+  final BuildContext parentContext;
+  const PixelGridWidget({Key? key, required this.parentContext}) : super(key: key);
 
   @override
   State<PixelGridWidget> createState() => _PixelGridWidgetState();
@@ -22,8 +23,8 @@ class _PixelGridWidgetState extends State<PixelGridWidget> {
 
   final TransformationController transformationController = TransformationController();
 
-  static const double cellWidth = 20.0;
-  static const double cellHeight = 20.0;
+  static const double cellWidth = PixelModel.pixelWidth;
+  static const double cellHeight = PixelModel.pixelHeight;
   static int rowCount = gridTest.length;
   static int columnCount = gridTest[0].length;
 
@@ -47,7 +48,7 @@ class _PixelGridWidgetState extends State<PixelGridWidget> {
   void initState() {
     super.initState();
     transformationController.addListener(onChangeTransformation);
-    transformationController.value.scale(2.5);
+    transformationController.value.scale(35.0);
   }
 
   @override
@@ -61,34 +62,36 @@ class _PixelGridWidgetState extends State<PixelGridWidget> {
   Widget build(BuildContext context) {
     return Center(
       child: InteractiveViewer.builder(
-        minScale: 1.5,//cellHeight / MediaQuery.of(context).size.height,
-        maxScale: 5,
+        minScale: 0.0001,//cellHeight / MediaQuery.of(context).size.height,
+        maxScale: 50,
         scaleEnabled: true,
-        boundaryMargin: const EdgeInsets.all(5),
+        boundaryMargin: EdgeInsets.zero,
         transformationController: transformationController,
         builder: (BuildContext context, vector.Quad viewport) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              for (int row = 0; row < rowCount; row++)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    for (int column = 0; column < columnCount; column++)
-                      isCellVisible(row: row, column: column, viewport: viewport)
-                        ? PixelWidget(
-                            pixelModel: gridTest[row][column],
-                            onTap: () {
-                              setState(() {
-                                gridTest[row][column] = PixelModel(color: Colors.black, username: '');
-                              });
-                            },
-                          )
-                        : const SizedBox(height: cellHeight, width: cellWidth)
-                  ],
-                )
-            ],
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                for (int row = 0; row < rowCount; row++)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for (int column = 0; column < columnCount; column++)
+                        isCellVisible(row: row, column: column, viewport: viewport)
+                          ? PixelWidget(
+                              pixelModel: gridTest[row][column],
+                              onTap: () {
+                                setState(() {
+                                  gridTest[row][column] = PixelModel(color: Colors.black, username: '');
+                                });
+                              },
+                            )
+                          : const SizedBox(height: cellHeight, width: cellWidth)
+                    ],
+                  )
+              ],
+            ),
           );
         },
       ),
