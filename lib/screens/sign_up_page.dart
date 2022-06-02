f84e2a27dfab06fc07a4bdef8398712e33423dcc
Hanri <henry018@laposte.net>
2022-06-02 10:46:59 +0200

    LogoWidget, SignUpScreen fixed layout

--- lib/screens/sign_up_page.dart
+++ lib/screens/sign_up_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pixel_war_app/bloc/services_bloc.dart';
 import 'package:pixel_war_app/widgets/button_widget.dart';
+import 'package:pixel_war_app/widgets/logo_widget.dart';
 import 'package:pixel_war_app/widgets/text_form_field_widget.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -15,45 +16,53 @@ class SignUpScreen extends StatelessWidget {
     final TextEditingController passwordController = TextEditingController();
     final TextEditingController passwordConfirmationController = TextEditingController();
 
-    return Center(
-      child: Form(
-        key: formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const SpacerWidget(),
-            TextFormFieldWidget(
-              parameters: EmailParameters(
-                controller: emailController
+    return Form(
+      key: formKey,
+      child: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          reverse: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const LogoWidget(),
+              const SpacerWidget(),
+              TextFormFieldWidget(
+                parameters: EmailParameters(
+                  controller: emailController
+                ),
               ),
-            ),
-            TextFormFieldWidget(
-              parameters: PasswordParameters(
-                controller: passwordController,
-                label: "Password",
-                hint: "your password"
+              const SpacerWidget(),
+              TextFormFieldWidget(
+                parameters: PasswordParameters(
+                  controller: passwordController,
+                  label: "Password",
+                  hint: "your password"
+                ),
               ),
-            ),
-            TextFormFieldWidget(
-              parameters: PasswordConfirmationParameters(
-                controller1: passwordConfirmationController,
-                controller2: passwordController,
-                label: "Password Confirmation",
-                hint: "same password"
+              const SpacerWidget(),
+              TextFormFieldWidget(
+                parameters: PasswordConfirmationParameters(
+                  controller1: passwordConfirmationController,
+                  controller2: passwordController,
+                  label: "Password Confirmation",
+                  hint: "same password"
+                ),
               ),
-            ),
-            const SpacerWidget(),
-            ButtonWidget(
-              onPressed: () {
-                FocusManager.instance.primaryFocus?.unfocus();
-                if (formKey.currentState!.validate()) {
-                  BlocProvider.of<ServicesBloc>(context).add(SignUpEvent(email: emailController.text, password: passwordController.text));
-                }
-              },
-              text: 'Sign Up'
-            )
-          ],
+              const SpacerWidget(),
+              ButtonWidget(
+                onPressed: () {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                  if (formKey.currentState!.validate()) {
+                    BlocProvider.of<ServicesBloc>(context).add(SignUpEvent(email: emailController.text, password: passwordController.text));
+                  }
+                },
+                text: 'Sign Up'
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/logo_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LogoWidget extends StatelessWidget {
+  const LogoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 100,
+      width: 100,
+      child: Placeholder()
+    );
+  }
+}
